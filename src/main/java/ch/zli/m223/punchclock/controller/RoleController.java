992f6b353e447c7fa8a291a111dd3c7d5cfac95e
Noel-Os <63641410+Noel-Os@users.noreply.github.com>
2021-09-29 13:51:02 +0200

    Methode createRole in RoleController hinzugefügt

--- src/main/java/ch/zli/m223/punchclock/controller/RoleController.java
+++ src/main/java/ch/zli/m223/punchclock/controller/RoleController.java
@@ -5,10 +5,7 @@ import ch.zli.m223.punchclock.service.RoleService;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
@@ -30,6 +27,13 @@ public class RoleController {
     @Path("/{id}")
     public Role getRole(@PathParam("id") long id) {
         return roleService.getRole(id);
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Role createRole(Role role) {
+        return roleService.createRole(role);
     }
 
 }
