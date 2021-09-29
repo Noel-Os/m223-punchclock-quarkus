5d317ba071a738f8ab7d4da63c9de65dbcc11b24
Noel-Os <63641410+Noel-Os@users.noreply.github.com>
2021-09-29 16:16:06 +0200

    AuthenticationService in groups mit Role verändert.

--- src/main/java/ch/zli/m223/punchclock/service/AuthenticationService.java
+++ src/main/java/ch/zli/m223/punchclock/service/AuthenticationService.java
@@ -1,17 +1,13 @@
 package ch.zli.m223.punchclock.service;
 
-import java.time.Duration;
-import java.util.Arrays;
-import java.util.HashSet;
+import ch.zli.m223.punchclock.domain.Role;
+import ch.zli.m223.punchclock.domain.User;
+import io.smallrye.jwt.build.Jwt;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
-
-import ch.zli.m223.punchclock.domain.Role;
-import ch.zli.m223.punchclock.domain.User;
-import io.smallrye.jwt.build.Jwt;
-import org.eclipse.microprofile.jwt.Claims;
+import java.time.Duration;
 
 @RequestScoped
 public class AuthenticationService {
@@ -19,26 +15,25 @@ public class AuthenticationService {
     @Inject
     private EntityManager entityManager;
 
-    public Boolean getUser(User user){
+    public Boolean getUser(User user) {
         var query = entityManager.createQuery("SELECT COUNT(*) FROM User u" +
-        " WHERE u.username=:username AND u.password=:password");
+                " WHERE u.username=:username AND u.password=:password");
         query.setParameter("username", user.getUsername());
         query.setParameter("password", user.getPassword());
         var result = query.getSingleResult();
-        if ((long) result == 1){
+        if ((long) result == 1) {
             return true;
         }
         return false;
     }
-    
-    public String GenerateValidJwtToken(String username, Role role){
+
+    public String GenerateValidJwtToken(String username, Role role) {
         String token =
-            Jwt.issuer("https://zli.ch/issuer") 
-            .upn(username)
-            .groups(new HashSet<>(Arrays.asList("User", "Admin")))
-            .claim(Claims.birthdate.name(), "2001-07-13")
-            .expiresIn(Duration.ofHours(1)) 
-            .sign();
+                Jwt.issuer("https://zli.ch/issuer")
+                        .upn(username)
+                        .groups(role.getRoleName())
+                        .expiresIn(Duration.ofHours(1))
+                        .sign();
         return token;
     }
 
