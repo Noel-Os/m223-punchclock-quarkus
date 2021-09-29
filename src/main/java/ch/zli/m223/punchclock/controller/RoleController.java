14209c9e18f47f752211dce73fcaacc415fafe32
Noel-Os <63641410+Noel-Os@users.noreply.github.com>
2021-09-29 13:35:31 +0200

    In RoleController den RoleService injected

--- src/main/java/ch/zli/m223/punchclock/controller/RoleController.java
+++ src/main/java/ch/zli/m223/punchclock/controller/RoleController.java
@@ -1,4 +1,16 @@
 package ch.zli.m223.punchclock.controller;
 
+import ch.zli.m223.punchclock.service.RoleService;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import javax.inject.Inject;
+import javax.ws.rs.Path;
+
+@Path("/roles")
+@Tag(name = "Roles", description = "Handling of roles")
 public class RoleController {
+
+    @Inject
+    RoleService roleService;
+
 }
