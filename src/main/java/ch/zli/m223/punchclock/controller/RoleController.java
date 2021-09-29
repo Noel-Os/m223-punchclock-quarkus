9e9c9973372877c56956a93d1e1d57342926eb27
Noel-Os <63641410+Noel-Os@users.noreply.github.com>
2021-09-29 13:37:33 +0200

    In RoleService den EntityManager injected

--- src/main/java/ch/zli/m223/punchclock/controller/RoleController.java
+++ src/main/java/ch/zli/m223/punchclock/controller/RoleController.java
@@ -1,10 +1,13 @@
 package ch.zli.m223.punchclock.controller;
 
+import ch.zli.m223.punchclock.domain.Role;
 import ch.zli.m223.punchclock.service.RoleService;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 @Path("/roles")
 @Tag(name = "Roles", description = "Handling of roles")
--- src/main/java/ch/zli/m223/punchclock/service/RoleService.java
+++ src/main/java/ch/zli/m223/punchclock/service/RoleService.java
@@ -1,4 +1,13 @@
 package ch.zli.m223.punchclock.service;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+@ApplicationScoped
 public class RoleService {
+
+    @Inject
+    EntityManager entityManager;
+
 }
