19af9eec809cd9735807874fff13651cc1d1261d
Noel-Os <63641410+Noel-Os@users.noreply.github.com>
2021-09-29 13:39:28 +0200

    Methode getRoles in RoleService hinzugefügt

--- src/main/java/ch/zli/m223/punchclock/service/RoleService.java
+++ src/main/java/ch/zli/m223/punchclock/service/RoleService.java
@@ -1,13 +1,22 @@
 package ch.zli.m223.punchclock.service;
 
+import ch.zli.m223.punchclock.domain.Role;
+import ch.zli.m223.punchclock.domain.User;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @ApplicationScoped
 public class RoleService {
 
     @Inject
     EntityManager entityManager;
+
+    public List<Role> getRoles() {
+        var query = entityManager.createQuery("FROM Role");
+        return query.getResultList();
+    }
 
 }
